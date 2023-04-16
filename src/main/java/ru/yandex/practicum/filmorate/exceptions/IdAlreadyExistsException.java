4e0e6c62089e9a2297fe0f46756facb432513d56
Иван Д <veroz1@mail.ru>
2023-04-16 11:21:05 +0500

    9ое ТЗ вер.1.2

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/IdAlreadyExistsException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/IdAlreadyExistsException.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.exceptions;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 
 @Getter
 @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
@@ -12,12 +10,6 @@ public class IdAlreadyExistsException extends RuntimeException {
     private String message;
 
     public IdAlreadyExistsException(Object o) {
-        message = "";
-        if (o instanceof User user) {
-            this.message = "Пользователь с таким id {} уже существует!: " + user.getId();
-        }
-        if (o instanceof Film film) {
-            this.message = "Фильм с таким id {} уже существует!: " + film.getId();
-        }
-    }
+       this.message = "Такой id уже существует!";
+            }
 }
--- src/main/java/ru/yandex/practicum/filmorate/exceptions/IdNotExistException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/IdNotExistException.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.exceptions;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 
 @Getter
 @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
@@ -12,11 +10,7 @@ public class IdNotExistException extends RuntimeException {
     private String message;
 
     public IdNotExistException(Object o) {
-        message = "";
-        if (o instanceof User user) {
-            this.message = "Не существует пользователя c id: " + user.getId();
-        } else if (o instanceof Film film) {
-            this.message = "Не найден фильм c id: " + film.getId();
-        }
+                   this.message = "Не существует пользователя/фильма таким c id!";
+
     }
 }
